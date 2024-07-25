9e950737f4cf50ffe492599fc2c05ebe3cb0028e
charfisirine <charfisirine62@gmail.com>
2024-07-25 12:27:47 +0100

    ajouter un fichier de configuration securityConfig to permmitter les methothdes http put delete post pour les articles

--- src/main/java/com/example/gestionstock/web/rest/ArticleRessource.java
+++ src/main/java/com/example/gestionstock/web/rest/ArticleRessource.java
@@ -1,10 +1,18 @@
 package com.example.gestionstock.web.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import com.example.gestionstock.repository.ArticleRepository;
 import com.example.gestionstock.domain.Article;
@@ -23,5 +31,25 @@ public class ArticleRessource {
     public Article findOne(@PathVariable Integer id){ 
         return articleRepository.findById(id).orElse(null);
     }
+    @PostMapping
+    public ResponseEntity<Article> addArticle(@RequestBody Article article) throws URISyntaxException{
+        Article result=articleRepository.save(article);
+        return ResponseEntity.created(new URI("/api/articles/" + result.getIdArticle())).body(result);
+    }
+    @PutMapping
+    public ResponseEntity<Article> updateArticle(@RequestBody Article article){
+        Article result=articleRepository.save(article);
+        return ResponseEntity.ok(result);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable Integer id) {
+        if (articleRepository.existsById(id)) {
+            articleRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
 }
 
